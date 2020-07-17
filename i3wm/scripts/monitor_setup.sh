0394af61f6a8dd1dd27a551a14582616eac59045
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-07-17 12:54:47 +0200

    feat(i3wm): add script for auto configuring monitor setup

---
+++ i3wm/scripts/monitor_setup.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+MACHINE=$(hostname)
+
+if [ $MACHINE == 'nixos' ]; then
+  xrandr \
+    --output "HDMI-1" --auto --primary \
+    --output "DVI-D-1" --auto --right-of "HDMI-1" \
+    --output "DVI-I-1" --auto --left-of "HDMI-1"
+fi
