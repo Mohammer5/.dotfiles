7d288279e06150bf05a295c88c7f8c4a522b8b71
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2021-11-10 17:02:14 +0100

    properly split multiple nix configurations

--- nix/configuration.laptop.nix
+++ nix/configuration.laptop.nix
@@ -7,29 +7,9 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      /etc/nixos/hardware-configuration.nix
+      /home/gerkules/.dotfiles/nix/shared.nix
     ];
-
-  # Use the systemd-boot EFI boot loader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-  boot = {
-    # Use the systemd-boot EFI boot loader.
-    loader = {
-      grub = {
-        efiSupport = true;
-        enable = true;
-        version = 2;
-        devices = [ "nodev" ];
-        useOSProber = true;
-      };
-
-      efi.canTouchEfiVariables = true;
-    };
-
-    # support exfat filesystem
-    # extraModulePackages = [ config.boot.kernelPackages.exfat-nofuse ];
-  };
 
   fileSystems = {
     "/shared" = {
@@ -37,180 +17,9 @@
     };
   };
  
-  networking.hostName = "LaptopNixOS"; # Define your hostname.
-
-  # Set your time zone.
-  time.timeZone = "Europe/Amsterdam";
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp0s25.useDHCP = true;
-  networking.interfaces.wlp3s0.useDHCP = true;
-  networking.networkmanager.enable = true;
-
-  hardware.bluetooth.enable = true;
-  hardware.sane.enable = true;
-  services.blueman.enable = true;
-
-  virtualisation.docker.enable = true;
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  nixpkgs.config.allowUnfree = true;
-
-  services.xserver = {
-    enable = true;
-    videoDrivers = [];
-
-    displayManager.defaultSession = "xfce+i3";
-    displayManager.lightdm = { enable = true; };
-
-    desktopManager = {
-      xterm.enable = false;
-      xfce = {
-        enable = true;
-        noDesktop = true;
-        enableXfwm = false;
-      };
-
-      gnome3.enable = true;
-    };
-
-    windowManager.i3 = {
-      enable = true;
-      package = pkgs.i3-gaps;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status
-        i3blocks
-      ];
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    neo4j
-    tree
-    htop
-    gnumake
-    cmake
-    python3
-    wget
-    # neovim needs extra libraries for system clipboard support
-    # (neovim.overrideAttrs (oldAttrs: {
-    #   buildInputs = (oldAttrs.buildInputs ++ [ pkgs.xorg.libX11 pkgs.xorg.libXt ]);
-    # }))
-    neovim
-    git
-    git-lfs
-    kitty
-    fzf
-    curl
-    firefox
-    chromium
-    yarn
-    docker-compose
-
-    # External HDD compatibility
-    ntfs3g
-    exfat-utils
-    p7zip
-
-    openvpn
-    update-resolv-conf
-    ledger-live-desktop
-
-    simple-scan
-    xorg.xbacklight
-    nyxt
-  ];
-
-  environment.etc.openvpn.source = "${pkgs.update-resolv-conf}/libexec/openvpn";
-
-  location = {
-    latitude = 49.0;
-    longitude = 8.0;
-  };
-
-  services = {
-    redshift = {
-      enable = true;
-    };
-
-    geoclue2 = {
-      enable = true;
-    };
-
-    localtime = {
-      enable = true;
-    };
-  };
-
-  programs.fish.enable = true;
-
-  environment.variables = {
-    CYPRESS_INSTALL_BINARY = "0";
-    CYPRESS_RUN_BINARY = "/home/gerkules/.dotfiles/nix/built-derivations/cypress-4.7.0/bin/Cypress";
-  };
-
-  users = {
-    groups = {
-      plugdev = {};
-      docker = {};
-    };
-
-    users.gerkules = {
-      isNormalUser = true;
-      extraGroups = [ "sudo" "docker" "plugdev" "scanner" "lp" ];
-      shell = pkgs.fish;
-      initialPassword = "gerkules";
-    };
-  };
-  nix.allowedUsers = ["gerkules"];
-
-  security.sudo.configFile = ''
-    # Keep SSH_AUTH_SOCK so that pam_ssh_agent_auth.so can do its magic.
-    Defaults env_keep+=SSH_AUTH_SOCK
-
-    # "root" is allowed to do anything.
-    root        ALL=(ALL:ALL) SETENV: ALL
-    gerkules    ALL=(ALL:ALL) SETENV: ALL
-
-    # extraRules
-    %wheel  ALL=(ALL:ALL)   SETENV: ALL
-
-    # Keep terminfo database for root and %wheel.
-    Defaults:root,%wheel env_keep+=TERMINFO_DIRS
-    Defaults:root,%wheel env_keep+=TERMINFO
-  '';
-
-  hardware.ledger.enable = true;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.printing.drivers = [
-    pkgs.gutenprint
-    pkgs.gutenprintBin
-    pkgs.brlaser
-    pkgs.brgenml1lpr
-    pkgs.brgenml1cupswrapper
-  ];
-  services.avahi.enable = true;
-  services.avahi.nssmdns = true;
-
-  # Enable sound.
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
-
-  hardware.opengl = {
-    enable = true;
-    driSupport32Bit = true;
+  networking = {
+    hostName = "LaptopNixOS"; # Define your hostname.
+    interfaces.wlp3s0.useDHCP = true;
   };
 
   # This value determines the NixOS release from which the default
@@ -220,25 +29,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "20.09"; # Did you read the comment?
-
-  # Required for Ledger Live to detect Ledger Nano S via USB
-  services.udev.extraRules = ''
-    # firmware 1.6.0+
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="2b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="3b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="4b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1807", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1808", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0000", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0001", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0004", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="1011", MODE="0660", GROUP="plugdev"
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="1015", MODE="0660", GROUP="plugdev"
-
-    # Rule for all ZSA keyboards
-    SUBSYSTEM=="usb", ATTR{idVendor}=="3297", GROUP="plugdev"
-    # Rule for the Moonlander
-    SUBSYSTEM=="usb", ATTR{idVendor}=="3297", ATTR{idProduct}=="1969", GROUP="plugdev"
-  '';
 }
